5e7c0cc592bcaf87b16ca98e23ff9b675adbc957
unknown <nandankabra2@gmail.com>
2022-02-24 11:20:00 +0530

    okok

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Header from './my components/Header';
 import {Todos} from './my components/Todos';
 import {Footer} from './my components/Footer';  
-import Test from './my components/Test';
-
-
+import React, { useState } from 'react';
 function App() {
+
   const onDelete=(todo)=>{
     console.log("i am delete o todo" , todo);
+    /* let index = todos.indexOf(todo);
+    todos.splice(index , 1); */
+    setTodos(todos.filter(()=>{
+      
+    }))
   }
-  let todos = [
+  const [todos, setTodos] = useState([
     { 
       sno: 1,
       title:"Go to the market",
@@ -25,7 +29,8 @@ function App() {
       title:"Go to the ghat",
       desc:"You need to go to the market  to get this job done3"
     }
-  ]
+  ]);
+
   return (
     <>
       <Header title="My Todo List"  searchBar={false}/>
